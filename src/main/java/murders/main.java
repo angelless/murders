95ef7218083dd9e4327849712647385d024489a3
angelless <dongkyu91622@gmail.com>
2016-11-23 09:34:56 +0900

    Create main.java

---
+++ src/main/java/murders/main.java
@@ -0,0 +1,118 @@
+package murders;
+
+import java.io.File;
+import java.util.HashSet;
+import java.util.Random;
+import java.util.Set;
+
+import cn.nukkit.Player;
+import cn.nukkit.Server;
+import cn.nukkit.event.EventHandler;
+import cn.nukkit.event.Listener;
+import cn.nukkit.event.entity.EntityDamageByEntityEvent;
+import cn.nukkit.event.entity.EntityDamageEvent;
+import cn.nukkit.event.player.PlayerInteractEvent;
+import cn.nukkit.item.Item;
+import cn.nukkit.level.Position;
+import cn.nukkit.plugin.PluginBase;
+import cn.nukkit.utils.Config;
+import cn.nukkit.utils.TextFormat;
+
+public class main extends PluginBase implements Listener {
+	public static Set<Position> gameSpawns = new HashSet<>();
+	public static Player murder;
+	public static Player hero;
+
+	@Override
+	public void onEnable() {
+		this.getLogger().info(TextFormat.colorize("&6Let's Play murder!"));
+		this.getServer().getPluginManager().registerEvents(this, this);
+		for (String pos : new Config(new File(this.getDataFolder(), "spawns.properties"), Config.PROPERTIES)
+				.getStringList("spawns")) {
+			main.gameSpawns.add(main.toStrFromPos(pos));
+		}
+
+	}
+
+	@Override
+	public void onDisable() {
+		Config config = new Config(new File(this.getDataFolder(), "spawns.properties"), Config.PROPERTIES);
+		HashSet<String> list = new HashSet<>();
+		for (Position str : main.gameSpawns) {
+			list.add(main.toPosFromStr(str));
+		}
+		config.set("spawns", list);
+		config.save();
+	}
+
+	public void start() {
+		main.murder = (Player) this.getServer().getOnlinePlayers().values().toArray()[rand(1,
+				this.getServer().getOnlinePlayers().size() - 1)];
+		main.hero = (Player) this.getServer().getOnlinePlayers().values().toArray()[rand(1,
+				this.getServer().getOnlinePlayers().size() - 1)];
+		if (murder.getName().equals(hero.getName())) {
+			start();
+		}
+		Server.getInstance().getOnlinePlayers().values().forEach((Player player) -> {
+			Position pos = (Position) main.gameSpawns.toArray()[main.rand(1, main.gameSpawns.size() - 1)];
+			player.teleport(pos);
+		});
+	}
+
+	public void stop(int type) {
+		if (type == 0) {
+			this.getServer().broadcastMessage(TextFormat.colorize("&b[생존팀 승리] &3생존팀이 승리하였습니다"));
+			for (Player player : this.getServer().getOnlinePlayers().values()) {
+				player.teleport(Server.getInstance().getDefaultLevel().getSpawnLocation());
+			}
+		} else {
+			this.getServer().broadcastMessage(TextFormat.colorize("&c[생존팀 승리] &4살인자가 승리하였습니다"));
+			for (Player player : this.getServer().getOnlinePlayers().values()) {
+				player.teleport(Server.getInstance().getDefaultLevel().getSpawnLocation());
+			}
+		}
+
+	}
+
+	@EventHandler
+	public void onSpawnAdd(PlayerInteractEvent e) {
+		if (e.getPlayer().isOp()) {
+			if (e.getItem().getId() == Item.CLAY) {
+				main.gameSpawns.add(e.getPlayer());
+			}
+		}
+	}
+
+	@EventHandler
+	public void onAtact(EntityDamageEvent event) {
+		if (event instanceof EntityDamageByEntityEvent) {
+			EntityDamageByEntityEvent event1 = (EntityDamageByEntityEvent) event;
+			if (event1.getDamager() instanceof Player && event1.getEntity() instanceof Player) {
+				if (event1.getDamager().equals(main.murder) && !event1.getEntity().equals(main.hero)) {
+					event1.getEntity().attack(30);
+					((Player)event1.getEntity()).sendMessage(TextFormat.colorize("&c[사망] &4당신은 머더러에게 사냥당하셨씁닌다"));
+					return;
+				}else if(!event1.getDamager().equals(main.hero)){
+					event1.setCancelled();
+					event.setCancelled();
+					return;
+					
+				}
+			}
+		}
+	}
+
+	public static int rand(int min, int max) {
+		return new Random().nextInt(max - min + 1) + min;
+	}
+
+	public static Position toStrFromPos(String str) {
+		return new Position(Integer.parseInt(str.split(",")[0]), Integer.parseInt(str.split(",")[1]),
+				Integer.parseInt(str.split(",")[2]), Server.getInstance().getLevelByName(str.split(",")[3]));
+	}
+
+	public static String toPosFromStr(Position pos) {
+		return new StringBuilder().append(pos.getFloorX() + ",").append(pos.getFloorY() + ",")
+				.append(pos.getFloorZ() + ",").append(pos.getLevel().getFolderName()).toString();
+	}
+}
